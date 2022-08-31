974a153bb04a6e01e075316ba3c6795076f39189
Aditya Kumar <mradityaroy001@gmail.com>
2022-08-31 15:36:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ app.secret_key = b'\xe5\xf1!\xde`\xf2\xd2vCh\xeeZ\xb5\x02\x02.'
 
 
 # Database
+# for remote web connection 
 # client = pymongo.MongoClient(r"mongodb+srv://aditya:Adi8877@cluster0.wjpj2.mongodb.net/?retryWrites=true")
 
+# for kubernetes deployment
 uri = "mongodb://"+"mongodb-service"+":"+str(27017)
 client = pymongo.MongoClient(uri)
 database = client['user_login_system']
 collection = database['users']
+
+# for docker deplotment
 # client = pymongo.MongoClient("mongodb://"+os.environ["MONGODB_SERVICE_PORT"],
 #                             username=os.environ["MONGO_INITDB_ROOT_USERNAME"],
 #                             password=os.environ["MONGO_INITDB_ROOT_PASSWORD"],
